40352202ef216c8df51bba394eb34b43310afb00
Luiz.Gomes <Luiz.cg.jr@outlook.com>
2022-03-02 01:10:55 -0300

    Abstraindo jogo de rpg

---
+++ JogoDeRpg/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JogoDeRpg.src.Entities;
+
+namespace JogoDeRpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Hero wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack());
+        }
+    }
+}
